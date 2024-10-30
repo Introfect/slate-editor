97f866bbf0c42e2e9bc036ddac3839bf6a454692
Aayushman Pratap <aayushmanpratap333@gmail.com>
2024-10-30 11:09:42 +0530

    fix ts issue

--- components/DragButton.tsx
+++ components/DragButton.tsx
@@ -1,32 +1,30 @@
 "use client";
+import { AlignJustify } from "lucide-react";
 import { forwardRef, useRef } from "react";
 import { useDrag } from "react-dnd";
 
 // eslint-disable-next-line react/display-name
 const DragButton = forwardRef<HTMLButtonElement, { onClick?: () => void }>(
   ({ onClick }) => {
-    const buttonRef = useRef<HTMLButtonElement>(null); // Create a ref for the button
+    const buttonRef = useRef<HTMLButtonElement>(null);
 
     const [{ isDragging }, drag] = useDrag({
       type: "BLOCK",
-      item: {
-        /* you can add any data you want here */
-      },
+      item: {},
       collect: (monitor) => ({
         isDragging: monitor.isDragging(),
       }),
     });
 
-    // Connect the button ref to the drag source
     drag(buttonRef);
 
     return (
       <button
-        ref={buttonRef} // Use the buttonRef
+        ref={buttonRef}
         onClick={onClick}
         style={{ opacity: isDragging ? 0.5 : 1, marginBottom: "5px" }}
       >
-        Drag Me
+        <AlignJustify />
       </button>
     );
   }
